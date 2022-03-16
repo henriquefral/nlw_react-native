7b685bd1b9a1a458a09d42a811e9a62998cb6f60
gessilt <henriquefreitas2710@gmail.com>
2022-03-16 08:46:37 -0300

    feat: new modal function

--- src/components/ModalView/index.tsx
+++ src/components/ModalView/index.tsx
@@ -3,7 +3,8 @@ import {
         View, 
         Text,
         Modal,
-        ModalProps 
+        ModalProps,
+        TouchableWithoutFeedback 
 } from 'react-native';
 
 import { styles } from './styles';
@@ -12,24 +13,30 @@ import { Background } from '../Background';
  
 type Props = ModalProps & {
     children: ReactNode;
+    closeModal:()=>void;
 }
 
 
-export function ModalView({children, ...rest}: Props) {
+export function ModalView({children, closeModal, ...rest}: Props) {
     return (
         <Modal
             transparent
             animationType='slide'
+            statusBarTranslucent
             {...rest}
         >
-            <View style={styles.overlay}> 
-                <View style={styles.container}> 
-                    <Background>
-                        <View style={styles.bar}/>
-                            {children}
-                    </Background>
+            <TouchableWithoutFeedback
+                onPress={closeModal}
+            > 
+                <View style={styles.overlay}> 
+                    <View style={styles.container}> 
+                        <Background>
+                            <View style={styles.bar}/>
+                                {children}
+                        </Background>
+                    </View>
                 </View>
-            </View>
+            </TouchableWithoutFeedback>
         </Modal>
     );
  
